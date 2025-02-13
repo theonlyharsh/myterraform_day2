1dcbe8e1b5f6a497be93f06bf72989f3346b0ec6
theonlyharsh <creator.zuni@gmail.com>
2025-02-13 06:02:27 +0000

    adding day 2 files

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+  description = "inserting the ami value"
+  type = string
+  default = "value"
+}
+variable "type" {
+  type = string
+  default = ""
+}
+variable "key"{
+    type = string
+    default = ""
+}
